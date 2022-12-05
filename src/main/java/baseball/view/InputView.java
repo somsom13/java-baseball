d3f305c5f11771173eb4018208a142360ce60c47
somsom13 <somin1113@naver.com>
2022-12-06 02:07:31 +0900

    refactor(InputView): 플레이어 입력 view 분리
    MVC 패턴을 적용하여 입력과 관련된 메소드를 View 객체로 분리한다.
    
    변경사항:
    - GamePrintConstant: 출력문 상수 클래스 생성
    - InputView: 플레이어 숫자 입력 및 숫자 구성 검증 메소드 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,29 @@
+package baseball.view;
+
+import baseball.stringenum.InputExceptionCode;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String NUMBER_REGEX = "[0-9]+";
+
+    public String readPlayerBallNumber() {
+        System.out.println(GamePrintConstant.NUMBER_INPUT);
+        return readNumbers(Console.readLine());
+    }
+
+    // input 이 너무 중복된다.
+    private String readNumbers(String input) {
+        validateOnlyString(input);
+        return input;
+    }
+
+    private void validateOnlyString(String input) {
+        if (isNoneNumberIncluded(input)) {
+            throw new IllegalArgumentException(InputExceptionCode.NOT_NUMBER_INCLUDE.toString());
+        }
+    }
+
+    private boolean isNoneNumberIncluded(String input) {
+        return !input.matches(NUMBER_REGEX);
+    }
+}
